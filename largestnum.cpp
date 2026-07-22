5d24516b5a54021196ca96d8d810153b5d11e778
Shatakshi1427 <shatakshijabade1427@gmail.com>
2026-07-22 10:57:42 +0530

    Add files via upload

---
+++ largestnum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int num1,num2;
+  cout<<"enter two numbers:";
+  cin>>num1>>num2;
+  if(num1>num2)
+  {
+  cout<<"number 1 is greater than number 2"<<num1<<endl;
+  }
+  else
+  {
+  cout<<"number 2 is greater than number 1"<<num2<<endl;
+  }
+  return 0;
+}
